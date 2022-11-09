ff2dd209138d16327a8f9e5006ca584777d35470
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-09 22:07:42 +0300

    add student

--- main.js
+++ main.js
@@ -31,3 +31,10 @@ console.log([...array])
 
 const copyCopyUser = {...user1, name: "Bob"}
 console.log(copyCopyUser)
+
+const student = {
+    name: "Antony",
+    age: 29,
+    friends: ["Ann", "Helge", "Versavia"]
+}
+
