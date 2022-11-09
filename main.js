d641c5800028ecc5099ca8d25d102b1146c30f5d
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-09 21:25:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let num1 = 5
+let num2 = num1
+num2 = 7
+console.log(num1, num2)
+
+let user1 = {
+    name: "Bob",
+    age: 29
+}
+let user2 = user1
+user2.name = "Alex"
+console.log(user1.name, user2.name)
+console.log(Number.isNaN(NaN)) // как вычислить NaN
+console.log(typeof []) // обманщик
+console.log(Array.isArray([])) // тру выдает
+console.log(Array.isArray({})) // false выдает //надо пересмотреть
